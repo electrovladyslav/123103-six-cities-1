bdae014d3020f20a9848644eb92c5bf1545dbd8c
electrovladyslav <electrovladyslav@gmail.com>
2019-05-09 13:02:45 +0300

    Сделал моки для карточек аренды (п. 1, 2)

---
+++ src/mocks/offers.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    isPremium: true,
+    imageSource: `img/apartment-01.jpg`,
+    price: 120,
+    isBookmarked: false,
+    rating: 4.5,
+    name: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`
+  },
+  {
+    isPremium: false,
+    imageSource: `img/room.jpg`,
+    price: 80,
+    isBookmarked: true,
+    rating: 4,
+    name: `Wood and stone place`,
+    type: `Private room`
+  },
+  {
+    isPremium: false,
+    imageSource: `img/apartment-02.jpg`,
+    price: 132,
+    isBookmarked: false,
+    rating: 4,
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`
+  },
+  {
+    isPremium: true,
+    imageSource: `img/apartment-03.jpg`,
+    price: 180,
+    isBookmarked: false,
+    rating: 5,
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`
+  }
+];
