b1bdd71e7d591822a8e671cf6ebdc6d694614e75
electrovladyslav <electrovladyslav@gmail.com>
2019-05-09 15:23:09 +0300

    Добавил ком-т карточку и отрендерил их в main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import RentCard from "../rent-card/rent-card.jsx";
+
 const Main = (props) => {
-  const rentNames = props.rentNames;
+  const offers = props.offers;
   const onCardTitleClick = props.onCardTitleClick;
 
   return (
@@ -138,61 +140,20 @@ const Main = (props) => {
                 */}
               </form>
               <div className="cities__places-list places__list tabs__content">
-
                 {/* ---Rent card--- */}
-                {rentNames.map((rentName) =>
-                  <article className="cities__place-card place-card" key = {rentName}>
-                    <div className="place-card__mark">
-                      <span>Premium</span>
-                    </div>
-                    <div className="cities__image-wrapper place-card__image-wrapper">
-                      <a href="#">
-                        <img
-                          className="place-card__image"
-                          src="img/apartment-01.jpg"
-                          width="260"
-                          height="200"
-                          alt="Place image"
-                        />
-                      </a>
-                    </div>
-                    <div className="place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;120</b>
-                          <span className="place-card__price-text">
-                            &#47;&nbsp;night
-                          </span>
-                        </div>
-                        <button
-                          className="place-card__bookmark-button button"
-                          type="button">
-                          <svg
-                            className="place-card__bookmark-icon"
-                            width="18"
-                            height="19">
-                            <use xlinkHref="#icon-bookmark" />
-                          </svg>
-                          <span className="visually-hidden">To bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{width: 93 + `%`}} />
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <h2 className="place-card__name" onClick={onCardTitleClick}>
-                        <a href="#">
-                          {rentName}
-                        </a>
-                      </h2>
-                      <p className="place-card__type">Apartment</p>
-                    </div>
-                  </article>
-                )}
+                {/* <RentCard
+                  offer = {offer}
+                /> */}
+                {offers.map((offer) => {
+                  return (
+                    <RentCard
+                      key={offer.name}
+                      offer={offer}
+                      onCardTitleClick={onCardTitleClick}
+                    />
+                  );
+                })}
                 {/* ---End of rent card--- */}
-
               </div>
             </section>
             <div className="cities__right-section">
@@ -205,10 +166,9 @@ const Main = (props) => {
   );
 };
 
-
 Main.propTypes = {
-  rentNames: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onCardTitleClick: PropTypes.func.isRequired,
+  offers: PropTypes.object.isRequired,
+  onCardTitleClick: PropTypes.func.isRequired
 };
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Main from "./components/main/main.jsx";
 
-const initialState = {
-  rentNames: [
-    `Beautiful & luxurious apartment at great location`,
-    `Wood and stone place`,
-    `Canal View Prinsengracht`,
-    `Nice, cozy, warm big bed apartment`,
-  ]
-};
+import Main from "./components/main/main.jsx";
+import offers from "./mocks/offers";
 
 const handleClick = (event) => {
   console.log(`The link was clicked.`); // eslint-disable-line no-console
   event.preventDefault();
 };
 
-const init = ({rentNames} = initialState) => {
+const init = () => {
 
   ReactDOM.render(<Main
-    rentNames = {rentNames}
+    offers = {offers}
     onCardTitleClick = {handleClick}
   />, document.getElementById(`root`));
 };
