c352fa918741e96df3a735d3fca636b009f7936a
electrovladyslav <electrovladyslav@gmail.com>
2019-05-12 08:08:32 +0300

    Подпилил е2е тест для main

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Main from './main';
+import React from "react";
+import Enzyme, {mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Main from "./main";
 
 Enzyme.configure({adapter: new Adapter()});
 
-it(`In main component click on card header work`, () => {
-  const onCardTitleClick = jest.fn();
+const onCardTitleClick = jest.fn();
+const mockOffers = [{
+  isPremium: false,
+  imageSource: `blabla.jpg`,
+  price: 100,
+  isBookmarked: false,
+  rating: 3,
+  name: `Super very best appartment 1`,
+  type: `Closet`,
+  activeCard: `activeCardId`
+}, {
+  isPremium: false,
+  imageSource: `blabla.jpg`,
+  price: 100,
+  isBookmarked: false,
+  rating: 3,
+  name: `Super very best appartment 2`,
+  type: `Closet`,
+  activeCard: `activeCardId`
+}, {
+  isPremium: false,
+  imageSource: `blabla.jpg`,
+  price: 100,
+  isBookmarked: false,
+  rating: 3,
+  name: `Super very best appartment 3`,
+  type: `Closet`,
+  activeCard: `activeCardId`
+}, {
+  isPremium: false,
+  imageSource: `blabla.jpg`,
+  price: 100,
+  isBookmarked: false,
+  rating: 3,
+  name: `Super very best appartment 4`,
+  type: `Closet`,
+  activeCard: `activeCardId`
+}];
 
-  const app = shallow(<Main
-    rentNames ={[`First`, `Second`, `Third`, `Fourth`]}
-    onCardTitleClick = {onCardTitleClick}
-  />);
+it(`In main component click on card header works`, () => {
+  const app = mount(
+      <Main offers={mockOffers} onCardTitleClick={onCardTitleClick} />
+  );
 
   const cardHeaders = app.find(`.place-card__name`);
   cardHeaders.forEach((cardHeader) => {
