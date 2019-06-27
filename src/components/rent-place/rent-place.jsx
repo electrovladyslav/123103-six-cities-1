42e18d46d08e556e0cd3ded780e111edc7b0bb90
electrovladyslav <electrovladyslav@gmail.com>
2019-06-27 19:50:45 +0300

    Добавил отзыв и список отзывов

--- src/components/rent-place/rent-place.jsx
+++ src/components/rent-place/rent-place.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import ReviewList from "../review-list/review-list.jsx";
 
 const RentPlace = (props) => {
   const {
@@ -48,7 +49,9 @@ const RentPlace = (props) => {
               <div className="property__name-wrapper">
                 <h1 className="property__name">{name}</h1>
                 <button
-                  className={`property__bookmark-button button ${isBookmarked ? `property__bookmark-button--active` : ``}`}
+                  className={`property__bookmark-button button ${
+                    isBookmarked ? `property__bookmark-button--active` : ``
+                  }`}
                   type="button">
                   <Link to="/login">
                     <svg
@@ -116,151 +119,35 @@ const RentPlace = (props) => {
                   <p className="property__text">{description}</p>
                 </div>
               </div>
-              <section className="property__reviews reviews">
-                <h2 className="reviews__title">
-                  Reviews &middot; <span className="reviews__amount">1</span>
-                </h2>
-                <ul className="reviews__list">
-                  <li className="reviews__item">
-                    <div className="reviews__user user">
-                      <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img
-                          className="reviews__avatar user__avatar"
-                          src="img/avatar-max.jpg"
-                          width="54"
-                          height="54"
-                          alt="Reviews avatar"
-                        />
-                      </div>
-                      <span className="reviews__user-name">Max</span>
-                    </div>
-                    <div className="reviews__info">
-                      <div className="reviews__rating rating">
-                        <div className="reviews__stars rating__stars">
-                          <span style={{width: rating * 20 + `%`}} />
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <p className="reviews__text">
-                        A quiet cozy and picturesque that hides behind a a river
-                        by the unique lightness of Amsterdam. The building is
-                        green and from 18th century.
-                      </p>
-                      <time className="reviews__time" dateTime="2019-04-24">
-                        April 2019
-                      </time>
-                    </div>
-                  </li>
-                </ul>
-                <form className="reviews__form form" action="#" method="post">
-                  <label
-                    className="reviews__label form__label"
-                    htmlFor="review">
-                    Your review
-                  </label>
-                  <div className="reviews__rating-form form__rating">
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="5"
-                      id="5-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="5-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="perfect">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
 
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="4"
-                      id="4-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="4-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="good">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="3"
-                      id="3-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="3-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="not bad">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="2"
-                      id="2-stars"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="2-stars"
-                      className="reviews__rating-label form__rating-label"
-                      title="badly">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      value="1"
-                      id="1-star"
-                      type="radio"
-                    />
-                    <label
-                      htmlFor="1-star"
-                      className="reviews__rating-label form__rating-label"
-                      title="terribly">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-                  </div>
-                  <textarea
-                    className="reviews__textarea form__textarea"
-                    id="review"
-                    name="review"
-                    placeholder="Tell how was your stay, what you like and what can be improved"
-                  />
-                  <div className="reviews__button-wrapper">
-                    <p className="reviews__help">
-                      To submit review please make sure to set{` `}
-                      <span className="reviews__star">rating</span> and describe
-                      your stay with at least{` `}
-                      <b className="reviews__text-amount">50 characters</b>.
-                    </p>
-                    <button
-                      className="reviews__submit form__submit button"
-                      type="submit"
-                      disabled="">
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </section>
+              <ReviewList
+                reviews={[
+                  {
+                    id: 1,
+                    user: {
+                      id: 4,
+                      isPro: false,
+                      name: `Max`,
+                      avatarUrl: `img/1.png`,
+                    },
+                    rating: 4,
+                    comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+                    date: `2019-04-09T14:13:56.569Z`,
+                  },
+                  {
+                    id: 2,
+                    user: {
+                      id: 5,
+                      isPro: false,
+                      name: `Alex`,
+                      avatarUrl: `img/1.png`,
+                    },
+                    rating: 4,
+                    comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+                    date: `2019-05-08T14:13:56.569Z`,
+                  },
+                ]}
+              />
             </div>
           </div>
           <section className="property__map map" />
---
+++ src/mocks/reviews.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    user: {
+      id: 4,
+      isPro: false,
+      name: `Max`,
+      avatarUrl: `img/1.png`
+    },
+    rating: 4,
+    comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`
+  },
+  {
+    id: 2,
+    user: {
+      id: 5,
+      isPro: false,
+      name: `Alex`,
+      avatarUrl: `img/1.png`
+    },
+    rating: 4,
+    comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    date: `2019-05-08T14:13:56.569Z`
+  }
+];
