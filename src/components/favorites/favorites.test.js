a5920514c2034f4d25751a635521b829f4cf0d73
electrovladyslav <electrovladyslav@gmail.com>
2019-07-02 16:40:53 +0300

    Подпилил тесты

--- src/components/favorites/favorites.test.js
+++ src/components/favorites/favorites.test.js
@@ -4,6 +4,8 @@ import renderer from "react-test-renderer";
 import Favorites from "./favorites.jsx";
 
 it(`Favorites component renders correctly`, () => {
-  const tree = renderer.create(<Favorites />).toJSON();
+  const tree = renderer
+    .create(<Favorites loadFavorites={jest.fn().mockImplementation(() => Promise.resolve())} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
