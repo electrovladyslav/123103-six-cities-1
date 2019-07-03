902fae023f83519929b03f6830c2f8f52df570c5
electrovladyslav <electrovladyslav@gmail.com>
2019-07-03 23:14:55 +0300

    Вынес стейт из boolmark в хок

--- src/components/bookmark/bookmark.jsx
+++ src/components/bookmark/bookmark.jsx
@@ -1,52 +1,34 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
-import {connect} from "react-redux";
-
-import {getAuthrizationStatus} from "../../selectors";
-import {Operation} from "../../reducer";
-import {BookmarkSize, RentCardClassesEnum} from "../../constants";
 
 class Bookmark extends PureComponent {
   constructor(props) {
     super(props);
-
-    this._handleClick = this._handleClick.bind(this);
-    this.bookmarkClass = props.bookmarkClass || RentCardClassesEnum.RENT_CARD;
   }
 
   render() {
-    const {isFavorite, isAuthorized} = this.props;
+    const {isFavorite, isAuthorized, bookmarkClass} = this.props;
     return (
       <button
-        className={`${this.bookmarkClass}__bookmark-button button ${
-          isFavorite ? `${this.bookmarkClass}__bookmark-button--active` : ``
+        className={`${bookmarkClass}__bookmark-button button ${
+          isFavorite ? `${bookmarkClass}__bookmark-button--active` : ``
         }`}
         type="button"
-        onClick={this._handleClick}>
+        onClick={this.props.onBookmarkClick}>
         {this._renderFlag(isAuthorized)}
       </button>
     );
   }
 
-  _handleClick(event) {
-    if (this.props.isAuthorized) {
-      const isAdding = this.props.isFavorite ? 0 : 1;
-      this.props
-        .postToFavorites(isAdding, this.props.offerId)
-        .then(this.props.onToggleFavoriteStatus(!!isAdding));
-    }
-    event.preventDefault();
-  }
-
   _renderFlag(isAuthorized) {
-    const bookmarkSize = this.props.bookmarkSize || BookmarkSize.NORMAL;
+    const {bookmarkSize, bookmarkClass} = this.props;
 
     if (isAuthorized) {
       return (
         <React.Fragment>
           <svg
-            className={`${this.bookmarkClass}__bookmark-icon`}
+            className={`${bookmarkClass}__bookmark-icon`}
             width={bookmarkSize.width}
             height={bookmarkSize.height}>
             <use xlinkHref="#icon-bookmark" />
@@ -58,7 +40,7 @@ class Bookmark extends PureComponent {
       return (
         <Link to="/login">
           <svg
-            className={`${this.bookmarkClass}__bookmark-icon`}
+            className={`${bookmarkClass}__bookmark-icon`}
             width={bookmarkSize.width}
             height={bookmarkSize.height}>
             <use xlinkHref="#icon-bookmark" />
@@ -76,26 +58,9 @@ Bookmark.propTypes = {
   bookmarkSize: PropTypes.shape({
     width: PropTypes.number,
     height: PropTypes.number,
-  }),
+  }).isRequired,
   bookmarkClass: PropTypes.string,
-  offerId: PropTypes.number,
-  postToFavorites: PropTypes.func,
-  onToggleFavoriteStatus: PropTypes.func,
+  onBookmarkClick: PropTypes.func,
 };
 
-const mapStateToProps = (state, ownProps) =>
-  Object.assign({}, ownProps, {
-    isAuthorized: getAuthrizationStatus(state),
-  });
-
-const mapDispatchToProps = (dispatch) => ({
-  postToFavorites: (status, offerId) =>
-    dispatch(Operation.postToFavorites(status, offerId)),
-});
-
-export {Bookmark};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Bookmark);
+export default Bookmark;
